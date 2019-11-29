34429c9b85f4a354c787e2bb4558214fb5bb261d
Fernando Barros <jbarros.1202@gmail.com>
2019-11-29 09:52:57 -0500

    Pantalla de derrota y de Premio

--- app/src/main/java/com/example/amst_primeraevalpractica_g2/Derrota.java
+++ app/src/main/java/com/example/amst_primeraevalpractica_g2/Derrota.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class premio extends AppCompatActivity {
+public class Derrota extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_premio);
+        setContentView(R.layout.activity_derrota);
         final Button boton_salida = (Button)findViewById(R.id.btnSalir);
         boton_salida.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +25,10 @@ public class premio extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void Principal (View view){
+        Intent i= new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
